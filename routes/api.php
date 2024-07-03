b0587f6d482b8b26c5af1ab21d105214bfe0340f
Bagas Saras <sarasbagas63@gmail.com>
2024-07-03 21:53:26 +0700

    Add route delivery

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\DeliveryController;
 use App\Http\Controllers\API\OrderController;
+use App\Models\Delivery;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +26,4 @@ Route::middleware('auth:sanctum')->group(function () {
 // Route::post('/customer', [CustomerController::class, 'store'])->name('customer');
 Route::apiResource('/customer', CustomerController::class);
 Route::apiResource('/order', OrderController::class);
+Route::apiResource('/delivery', DeliveryController::class);
